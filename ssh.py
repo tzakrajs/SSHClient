557b23e922634ccea3d9e030383d4a76e67973f1
Thomas Zakrajsek <tzakrajs@gmail.com>
2012-04-23 22:53:41 +0000

    Added Apache 2.0 License

--- ssh.py
+++ ssh.py
@@ -1,3 +1,17 @@
+# Copyright [2012] [Thomas Zakrajsek]
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 import paramiko
 import re
 import time
@@ -41,6 +55,13 @@ class SSHShell(object):
             private_key = paramiko.RSAKey.from_private_key_file(private_key_file)
         else:
             private_key = None
+        if self.verbose:
+            logger = new_logger('worker', 'debug')
+        if self.debug:
+            logger = new_logger('worker', 'info')
+        else:
+            logger = new_logger('worker', 'warning')
+
         self.logger = logging.getLogger('worker')
         self.logger.debug("Defaulting to port 22")
         (self.hostname, self.username, self.password) = (host, un, pw)
